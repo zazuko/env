7260f6ce7acc0452e460e5035e54dea5350f198e
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-07-24 11:31:29 +0200

    chore: eslint and build

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -2,16 +2,15 @@ import Environment from '@rdfjs/environment'
 import DatasetFactory from '@rdfjs/environment/DataFactory.js'
 import DataFactory from '@rdfjs/environment/DatasetFactory.js'
 import FormatsFactory from '@rdfjs/environment/FormatsFactory.js'
-import NsBuildersFactory from '@tpluscode/rdf-ns-builders';
-import ClownfaceFactory from 'clownface/Factory.js'
+import NsBuildersFactory from '@tpluscode/rdf-ns-builders'
+// import ClownfaceFactory from 'clownface/Factory.js'
 
-const env =  new Environment([
-    DataFactory,
-    DatasetFactory,
-    FormatsFactory,
-    NsBuildersFactory,
-    ClownfaceFactory
+const env = new Environment([
+  DataFactory,
+  DatasetFactory,
+  FormatsFactory,
+  NsBuildersFactory,
+  // ClownfaceFactory,
 ])
-
 
 export default env
