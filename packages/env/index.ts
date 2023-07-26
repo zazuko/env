c5571feaa15cdcaed41d0104d567d7bb33513b61
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-07-26 09:27:11 +0200

    feat: add clownface factory

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -3,14 +3,14 @@ import DatasetFactory from '@rdfjs/environment/DataFactory.js'
 import DataFactory from '@rdfjs/environment/DatasetFactory.js'
 import FormatsFactory from '@rdfjs/environment/FormatsFactory.js'
 import NsBuildersFactory from '@tpluscode/rdf-ns-builders'
-// import ClownfaceFactory from 'clownface/Factory.js'
+import ClownfaceFactory from 'clownface/Factory.js'
 
 const env = new Environment([
   DataFactory,
   DatasetFactory,
   FormatsFactory,
   NsBuildersFactory,
-  // ClownfaceFactory,
+  ClownfaceFactory,
 ])
 
 export default env
