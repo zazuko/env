e0ef9aa57cb164dcdcb516502746c2dffd53645b
tpluscode <tpluscode@users.noreply.github.com>
2023-07-29 15:55:15 +0200

    feat: add environment factory method

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,16 +1,25 @@
 import Environment from '@rdfjs/environment'
 import DatasetFactory from '@rdfjs/environment/DataFactory.js'
 import DataFactory from '@rdfjs/environment/DatasetFactory.js'
+import NamespaceFactory from '@rdfjs/environment/NamespaceFactory.js'
 import FormatsFactory from '@rdfjs/environment/FormatsFactory.js'
 import NsBuildersFactory from '@tpluscode/rdf-ns-builders'
 import ClownfaceFactory from 'clownface/Factory.js'
 
-const env = new Environment([
-  DataFactory,
-  DatasetFactory,
-  FormatsFactory,
-  NsBuildersFactory,
-  ClownfaceFactory,
-])
+interface FactoryConstructor<F = {}> {
+  new(...args: any[]): F
+}
 
-export default env
+export function create<F extends FactoryConstructor>(...additionalFactories: F[]) {
+  return new Environment([
+    DataFactory,
+    DatasetFactory,
+    FormatsFactory,
+    NamespaceFactory,
+    NsBuildersFactory,
+    ClownfaceFactory,
+    ...additionalFactories,
+  ])
+}
+
+export default create()
---
+++ packages/env/test/index.test.ts
@@ -0,0 +1,69 @@
+import { expect } from 'chai'
+import $rdf from 'rdf-ext'
+import { create } from '../index.js'
+
+describe('@zazuko/env', () => {
+  describe('create', () => {
+    let env: ReturnType<typeof create>
+
+    context('without additional factories', () => {
+      before(() => {
+        env = create()
+      })
+
+      testStandardFactories()
+    })
+
+    context('with additional factory', () => {
+      class Factory {
+        get foo() {
+          return 'bar'
+        }
+
+        static get exports() {
+          return ['foo']
+        }
+      }
+
+      before(() => {
+        env = create(Factory)
+      })
+
+      it('adds that factory', () => {
+        // when
+        const env = create(Factory)
+
+        // then
+        expect(env.foo).to.eq('bar')
+      })
+
+      testStandardFactories()
+    })
+
+    function testStandardFactories() {
+      it('includes include clownface factory', () => {
+        expect(env.clownface().namedNode('http://example.com/foo').term).to.deep.eq($rdf.namedNode('http://example.com/foo'))
+      })
+
+      it('includes dataset factory', () => {
+        expect(env.dataset().size).to.eq(0)
+      })
+
+      it('includes data factory', () => {
+        expect(env.namedNode('http://example.com/foo')).to.deep.eq($rdf.namedNode('http://example.com/foo'))
+      })
+
+      it('includes namespace factory', () => {
+        expect(env.namespace('http://example.com/').foo).to.deep.eq($rdf.namedNode('http://example.com/foo'))
+      })
+
+      it('includes formats factory', () => {
+        expect(env.formats.serializers.import('text/turtle', $rdf.dataset().toStream())).to.be.null
+      })
+
+      it('includes ns builders factory', () => {
+        expect(env.ns.schema.Person).to.deep.eq($rdf.namedNode('http://schema.org/Person'))
+      })
+    }
+  })
+})
