ff2a9389c62eed5d3bbd5c5029b9eab2b5a4b809
tpluscode <tpluscode@users.noreply.github.com>
2023-07-30 18:49:04 +0200

    build(deps): update @types/rdfjs__environment

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,14 +1,11 @@
 import Environment from '@rdfjs/environment'
+import type { FactoryConstructor } from '@rdfjs/environment/Environment.js'
 import DatasetFactory from '@rdfjs/environment/DataFactory.js'
 import DataFactory from '@rdfjs/environment/DatasetFactory.js'
 import NamespaceFactory from '@rdfjs/environment/NamespaceFactory.js'
 import FormatsFactory from '@rdfjs/environment/FormatsFactory.js'
 import NsBuildersFactory from '@tpluscode/rdf-ns-builders'
 import ClownfaceFactory from 'clownface/Factory.js'
-
-interface FactoryConstructor<F = object> {
-  new(...args: unknown[]): F
-}
 
 export function create<F extends FactoryConstructor>(...additionalFactories: F[]) {
   return new Environment([
