b06db23876557aba1284b7e9a4ff3532505cd577
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2024-03-22 12:08:27 +0100

    feat: web-friendly module

--- packages/env/index.ts
+++ packages/env/index.ts
@@ -1,28 +1,11 @@
-import DataFactory from '@rdfjs/data-model/Factory.js'
-import NamespaceFactory from '@rdfjs/namespace/Factory.js'
-import FormatsFactory from '@rdfjs/formats/Factory.js'
-import TermMapFactory from '@rdfjs/term-map/Factory.js'
-import TermSetFactory from '@rdfjs/term-set/Factory.js'
-import NsBuildersFactory from '@tpluscode/rdf-ns-builders'
-import ClownfaceFactory from 'clownface/Factory.js'
-import TraverserFactory from '@rdfjs/traverser/Factory.js'
 import Environment from './Environment.js'
 import DatasetFactory from './lib/DatasetFactory.js'
+import parent from './web.js'
 
 export type { DerivedEnvironment } from '@zazuko/env-core/lib/extend.js'
 
 export function create() {
-  return new Environment([
-    DataFactory,
-    DatasetFactory,
-    FormatsFactory,
-    NamespaceFactory,
-    NsBuildersFactory,
-    ClownfaceFactory,
-    TermMapFactory,
-    TermSetFactory,
-    TraverserFactory,
-  ])
+  return new Environment([DatasetFactory], { parent })
 }
 
 export default create()
